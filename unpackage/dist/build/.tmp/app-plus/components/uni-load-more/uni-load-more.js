30ce0b179c49f9d138720c5ee6832abe637aeb20
ZhaofaYong <122621761@qq.com>
2020-02-18 21:46:37 +0800

    这是第一次修改提交

---
+++ unpackage/dist/build/.tmp/app-plus/components/uni-load-more/uni-load-more.js
@@ -0,0 +1,10 @@
+(global["webpackJsonp"]=global["webpackJsonp"]||[]).push([["components/uni-load-more/uni-load-more"],{"18d4":function(t,n,e){"use strict";e.r(n);var o=e("ca80"),u=e("69a1");for(var a in u)"default"!==a&&function(t){e.d(n,t,function(){return u[t]})}(a);e("f8ae");var r,c=e("f0c5"),f=Object(c["a"])(u["default"],o["b"],o["c"],!1,null,null,null,!1,o["a"],r);n["default"]=f.exports},"69a1":function(t,n,e){"use strict";e.r(n);var o=e("9f91"),u=e.n(o);for(var a in o)"default"!==a&&function(t){e.d(n,t,function(){return o[t]})}(a);n["default"]=u.a},"9f91":function(t,n,e){"use strict";Object.defineProperty(n,"__esModule",{value:!0}),n.default=void 0;var o={name:"uni-load-more",props:{status:{type:String,default:"more"},showIcon:{type:Boolean,default:!0},color:{type:String,default:"#777777"},contentText:{type:Object,default:function(){return{contentdown:"上拉显示更多",contentrefresh:"正在加载...",contentnomore:"没有更多数据了"}}}},data:function(){return{}}};n.default=o},a950:function(t,n,e){},ca80:function(t,n,e){"use strict";var o,u=function(){var t=this,n=t.$createElement;t._self._c},a=[];e.d(n,"b",function(){return u}),e.d(n,"c",function(){return a}),e.d(n,"a",function(){return o})},f8ae:function(t,n,e){"use strict";var o=e("a950"),u=e.n(o);u.a}}]);
+;(global["webpackJsonp"] = global["webpackJsonp"] || []).push([
+    'components/uni-load-more/uni-load-more-create-component',
+    {
+        'components/uni-load-more/uni-load-more-create-component':(function(module, exports, __webpack_require__){
+            __webpack_require__('6e42')['createComponent'](__webpack_require__("18d4"))
+        })
+    },
+    [['components/uni-load-more/uni-load-more-create-component']]
+]);
